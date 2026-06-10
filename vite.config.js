945a28bd969a77cc8e123a022fb56b8b14c3fd37
PropositoFranco <soyfrancocontupotencial@gmail.com>
2026-06-10 02:42:11 -0600

    simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -9,23 +9,4 @@ export default defineConfig({
     port: 5173,
     allowedHosts: ['daydream-ooze-celtic.ngrok-free.dev']
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
-              return 'vendor-react'
-            }
-            if (id.includes('@supabase')) {
-              return 'vendor-supabase'
-            }
-            if (id.includes('stripe')) {
-              return 'vendor-stripe'
-            }
-          }
-        }
-      }
-    }
-  }
 })
